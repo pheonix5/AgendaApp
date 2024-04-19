799f039c0a19afabc99b1ae165fa4ad01eed2ddb
pheonix5 <lucas.felix413@gmail.com>
2024-04-19 08:02:19 -0300

    Create: :bricks: Create project ands add libs

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/**/*.{ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
